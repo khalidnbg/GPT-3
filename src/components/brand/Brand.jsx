1de83f2cc0005ed5f9dc119a0ffe36a92edad499
khalidnbg <khalidnabgaoui4@gmail.com>
2024-04-18 11:38:42 +0100

    set!

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,24 @@
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
+import "./brand.css";
+
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} alt="brand" />
+    </div>
+    <div>
+      <img src={slack} alt="brand" />
+    </div>
+    <div>
+      <img src={atlassian} alt="brand" />
+    </div>
+    <div>
+      <img src={dropbox} alt="brand" />
+    </div>
+    <div>
+      <img src={shopify} alt="brand" />
+    </div>
+  </div>
+);
+
+export default Brand;
